7c4ab8c001705d183e20387f3e1dc461bef4fc0d
deon <deon.humank6@gmail.com>
2024-04-25 19:34:48 +0200

    made slight updates to python_selenium_tests/UI_test.py
    added python_selenium_tests/UI_test_swapi_dev.py as this one is using https:/swapi.dev as per the original requirements but I had certificate issues trying to browse / use this
    api in my tests

--- python_selenium_tests/UI_test_swapi_dev.py
+++ python_selenium_tests/UI_test_swapi_dev.py
@@ -6,7 +6,6 @@ import time
 import unittest
 import requests
 
-#using the chrome webdriver and navigating to the localhost address where the front end application is running on port 3000
 driver = webdriver.Chrome()
 driver.get("http://localhost:3000/")
 
@@ -15,12 +14,11 @@ driver.get("http://localhost:3000/")
 
 # UI test 1 
 print("this is UI test 1")
-#waiting so that all elements are loaded on the page
+
 time.sleep(3)
 
 title_heading = driver.find_element(By.XPATH, '/html/body/section/main/table/thead/tr/th[1]').click()
 
-# waiting after the click / sorting of the title column so that all elements are loaded on the page
 time.sleep(3)
 
 #checking the name of the last item in the list is equal to "The Phantom Menace"
@@ -30,7 +28,7 @@ def last_item(self):
         secondValue = driver.find_element(By.XPATH, '/html/body/section/main/table/tbody/tr[6]/td[1]/a').text
         # error message in case of test case failure
         message = "The last value are not equal to The Phantom Menace !"
-        # assertEqual() to check if the first & second value are the same
+        # assertEqual() to check equality of first & second value 
         self.assertEqual(firstValue, secondValue, message)
 
 
@@ -57,7 +55,8 @@ def wookie(self):
         
         
         
-                
+        
+        
                
         
 
@@ -93,14 +92,12 @@ def camino(self):
 # API test 1 : get the list of movies and assert that the count of films is equal to 6
 print("this is API test 1")
 
-#browsing to the api page which returns all of the films
-response = requests.get('https://swapi.py4e.com//api/films')
-# assigning the response from the get api call to a variable to that i can assert for a value in the response, 
+response = requests.get('https://swapi.dev/api/films')
 api_response_text = response.text
 
-#print (api_response_text) , I was using this for troublshooting, to see the response data being returned on my api get request
-# had to use an escape character \ to be able to assert for the exact text "count" : 7 , otherwise you will be running into syntax issues. I did a count of 7 as that is the number of films returned on this specific api used
-expected_text = "\"count\":7"
+#print (api_response_text)
+# had to use an escape character \ to be able to assert for the exact text "count" : 6 , otherwise you will be running into syntax issues.
+expected_text = "\"count\":6"
 
 # also adding a message here if the assert text is not present in the response value that I get from the api
 assert expected_text in api_response_text, f"Text '{expected_text}' not found!"
@@ -112,11 +109,10 @@ assert expected_text in api_response_text, f"Text '{expected_text}' not found!"
 
 # API test 2 : get the 3rd movie and check if the director is  ‘Richard Marquand’
 print("this is API test 2")
-response = requests.get('https://swapi.py4e.com//api/films/3')
+response = requests.get('https://swapi.dev/api/films/3')
 api_response_text = response.text
 
 expected_text = "\"director\":\"Richard Marquand\""  
-# also adding a message here if the assert text is not present in the response value that I get from the api
 assert expected_text in api_response_text, f"Text '{expected_text}' not found!"
 
 
@@ -126,12 +122,11 @@ assert expected_text in api_response_text, f"Text '{expected_text}' not found!"
 
 # API test 3 : get the 5th move and assert that that the ’Producers’ are not ‘Gary Kurtz, George Lucas'
 print("this is API test 3")
-response = requests.get('https://swapi.py4e.com//api/films/5')
+response = requests.get('https://swapi.dev/api/films/5')
 api_response_text = response.text
 
 expected_text1 = "\"producer\": \"Gary Kurtz\""  
 expected_text2 = "\"director\":\"George Lucas1\""
-# also adding a message here if the assert text is not present in the response value that I get from the api
 assert expected_text1 not in api_response_text, f"Text '{expected_text1}' found!"
 assert expected_text1 not in api_response_text, f"Text '{expected_text2}' found!"
 
